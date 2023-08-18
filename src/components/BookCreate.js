b84519482bee0ea021bc666c4171044ae804db39
Ruth <ruthcassidy1221@gmail.com>
2023-08-18 14:09:16 +0100

    Add book create form with state updates on submit

--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,6 +1,27 @@
-function BookCreate() {
+import { useState } from "react";
+
+function BookCreate({onCreate}) {
+    const [title, setTitle] = useState('');
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onCreate(title);
+        setTitle("");
+    }
+
     return (
-        <div>Book Create</div>;
+        <div>
+            <h1>Add A Book</h1>
+            <form onSubmit={handleSubmit}>
+                <label>Title</label><br/>
+                <input value={title} onChange={handleChange} /><br/>
+                <button>Add</button>
+            </form>
+        </div>
     )
 }
 
