ebe769a0ac60e4825fb102181482dc7493facfa6
Ruth <ruthcassidy1221@gmail.com>
2023-08-22 12:52:55 +0100

    Add delete functionality

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
+  const deleteBook = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+
+    setBooks(updatedBooks);
+  };
+
   const createBook = (title) => {
     const updatedBooks = [...books, {id: Math.round(Math.random() * 1000), title}]
     setBooks(updatedBooks);
-    console.log(books);
   };
 
   return (
     <div className="app">
-      <BookList books={books} />
+      <BookList books={books} onDelete={deleteBook} />
       <BookCreate onCreate={createBook} />
     </div>
   );
